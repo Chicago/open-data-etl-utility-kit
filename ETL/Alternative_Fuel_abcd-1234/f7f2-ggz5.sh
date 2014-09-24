1ad4b4c4a69da0c01c995b170ba4f934543b2343
Tom Schenk Jr <tomschenkjr@gmail.com>
2014-09-24 10:56:04 -0700

    Removed example folder, to be used in tutorial instead

--- ETL/Alternative_Fuel_abcd-1234/f7f2-ggz5.sh
+++
@@ -1,9 +0,0 @@
-. /home/svc_pentaho/ETL/ETL_Setup.sh
-
-ETL_NAME=Alternative_Fuel_f7f2-ggz5
-ETL_DIR_RELATIVE=Alternative_Fuel_f7f2-ggz5/
-DATA_SYNC_ETL=Y
-
-echo "ETL $ETL_NAME has started at $NOW."
-
-. $ETL_ROOT_DIR/Standard_ETL.sh $ETL_NAME $ETL_DIR_RELATIVE $DATA_SYNC_ETL 

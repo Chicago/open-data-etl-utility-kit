75bf4d3ceb4f1605c3898a776b15f14a8e4d2e82
Jonathan Levy <jonathan.levy@cityofchicago.org>
2014-11-06 16:53:25 -0600

    Updated A_TodayLogs.sh to take a -e parameter to exclude specified datasets by 4x4. These will generally be datasets that run frequently and would clutter up the output.

--- Log/A_TodayLogs.sh
+++ Log/A_TodayLogs.sh
@@ -1,1 +1,20 @@
-ls -l | grep $(date +"%Y%m%d")
+#Specify the 4x4 values to exclude, separated by pipe characters.
+DATASETS_TO_EXCLUDE="n4j6-wkkf|t2qc-9pjd|qmqz-2xku|97wa-y6ff"
+
+if [ -z "$1" ]  # No parameter. Run the regular grep.
+then
+   ls -l | grep $(date +"%Y%m%d")
+   exit 0
+else
+   case $1 in
+      -e)  #Exclude specified 4x4 values
+         ls -l | grep $(date +"%Y%m%d") | grep -Ev $DATASETS_TO_EXCLUDE
+         exit 0
+         ;;
+      *)   #Anything else
+         echo "Usage: A_Today.sh [-e]"
+         echo "   -e Exclude a pre-specified set of datasets by 4x4, generally those that run frequently and would clutter the output."
+         exit 1
+         ;;
+   esac
+fi

f4f2a3b25fd6c138b057e4e45cc49a7b1482001e
Jef Waltman <jef@lfaccess.net>
2015-10-16 09:24:40 -0500

    Updated to run the command rather than simply echo

--- Log/A_RunETL.bat
+++ Log/A_RunETL.bat
@@ -1,6 +1,6 @@
 @echo off
 IF [%1] == [] GOTO USAGE
-schtasks /Query /FO list /V | FOR /F "tokens=2 DELIMS=:" %%f in ('findstr /RC:"^Task To Run"') do @FOR /F "tokens=* delims= " %%a in ('@echo %%f') do @echo %%a | find %1%"
+schtasks /Query /FO list /V FOR /F "tokens=2 DELIMS=:" %%f in ('findstr /RC:"^Task To Run"') do @FOR /F "tokens=* delims= " %%a in ('@echo %%f') do @echo %%a | FOR /F "tokens=*" %%b in ('find "%1"') do @call %%b
 exit /B
 
 :USAGE

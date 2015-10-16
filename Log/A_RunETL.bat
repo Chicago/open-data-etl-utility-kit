7bd5cc2847cd3d14930268401d15f0b1c742b3cb
Jef Waltman <jef@lfaccess.net>
2015-10-16 11:36:48 -0500

    Added parameter validation

--- Log/A_RunETL.bat
+++ Log/A_RunETL.bat
@@ -1,6 +1,14 @@
 @echo off
 IF [%1] == [] GOTO USAGE
-schtasks /Query /FO list /V FOR /F "tokens=2 DELIMS=:" %%f in ('findstr /RC:"^Task To Run"') do @FOR /F "tokens=* delims= " %%a in ('@echo %%f') do @echo %%a | FOR /F "tokens=*" %%b in ('find "%1"') do @call %%b
+
+REM Sanity check
+echo %1 | findstr /r "\<[a-Z0123456789][a-Z0123456789][a-Z0123456789][a-Z0123456789]-[a-Z0123456789][a-Z0123456789][a-Z0123456789][a-Z0123456789]\>"
+IF %ERRORLEVEL% NEQ 0 (
+  echo Invalid Dataset4x4
+  exit /B 1
+)
+
+schtasks /Query /FO list /V | FOR /F "tokens=2 DELIMS=:" %%f in ('findstr /RC:"^Task To Run"') do @FOR /F "tokens=* delims= " %%a in ('@echo %%f') do @echo %%a | FOR /F "tokens=*" %%b in ('find "%1"') do @call %%b
 exit /B
 
 :USAGE
